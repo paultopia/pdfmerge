063cd076662ae23d9a07d1e1dc7ad01f7946908f
Paul Gowder <paul.gowder@gmail.com>
2019-01-13 17:43:14 -0600

    attempt to fix throwing oops

--- Sources/swiftpdfcore/openpdf.swift
+++ Sources/swiftpdfcore/openpdf.swift
@@ -4,7 +4,7 @@ import Foundation
 
 public func copyData(infile: String, outfile: String) {
     let pdf = NSData(contentsOfFile: infile)
-    pdf!.write(toFile: outfile)
+    try! pdf!.write(toFile: outfile)
     }
 
 
