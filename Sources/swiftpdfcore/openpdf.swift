b851a449b3eb8e7ac6efa537455bb6297c30ea37
Paul Gowder <paul.gowder@gmail.com>
2019-01-13 17:42:33 -0600

    attempt to fix optional oops

--- Sources/swiftpdfcore/openpdf.swift
+++ Sources/swiftpdfcore/openpdf.swift
@@ -3,8 +3,8 @@ import Foundation
 
 
 public func copyData(infile: String, outfile: String) {
-    let pdf = try! NSData(contentsOfFile: infile)
-    pdf.write(toFile: outfile)
+    let pdf = NSData(contentsOfFile: infile)
+    pdf!.write(toFile: outfile)
     }
 
 
