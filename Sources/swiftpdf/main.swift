256b0b9612e6326108c794e075c4342d6805dec9
Paul Gowder <paul.gowder@gmail.com>
2019-01-13 20:13:49 -0600

    cleanup old code

--- Sources/swiftpdf/main.swift
+++ Sources/swiftpdf/main.swift
@@ -4,34 +4,3 @@ let merger = PDFMerger()
 
 merger.run()
 
-// let pdfs = ["page1.pdf", "long1.pdf", "long2.pdf", "page2.pdf"]
-// let mergedfile = "merged.pdf"
-// let merged = mergePDFs(files: pdfs)
-// merged.write(toFile: mergedfile)
-
-// let long1 = "long1.pdf"
-// let long2 = "long2.pdf"
-
-// let mergedfile = "merged.pdf"
-
-// let merged = mergeTwoLongPDFs(file1: long1, file2: long2)
-
-// merged.write(toFile: mergedfile)
-
-
-// let pdf1 = "page1.pdf"
-// let pdf2 = "page2.pdf"
-
-// let mergedfile = "merged.pdf"
-
-// let merged = mergeSinglePagePDFs(file1: pdf1, file2: pdf2)
-
-// merged.write(toFile: mergedfile)
-
-// print(readPDF(infile: pdf1))
-
-// let outfile = "pdfout.pdf"
-
-// copyData(infile: pdf1, outfile: outfile)
-
-// print("Hello, world!")
--- Sources/swiftpdfcore/openpdf.swift
+++ Sources/swiftpdfcore/openpdf.swift
@@ -2,47 +2,6 @@ import Quartz
 import Foundation
 
 
-public func copyData(infile: String, outfile: String) {
-    let pdf = NSData(contentsOfFile: infile)
-    try! pdf!.write(toFile: outfile)
-    // apparently, and contrary to the docs,
-    // the NSData initializer returns an optional, and the write function throws.
-    }
-
-public func readPDF(infile: String) -> String {
-    let pdata = try! NSData(contentsOfFile: infile) as Data
-    // but it DOES throw when you try to force cast it with as.  special...
-    let pdf = PDFDocument(data: pdata)
-    return pdf!.string!
-}
-
-public func mergeSinglePagePDFs(file1: String, file2: String) -> PDFDocument {
-    let pdata1 = try! NSData(contentsOfFile: file1) as Data
-    let pdata2 = try! NSData(contentsOfFile: file2) as Data
-    let pdf1 = PDFDocument(data: pdata1)
-    let pdf2 = PDFDocument(data: pdata2)
-    print(pdf1!.pageCount)
-    let page2 = pdf2!.page(at: 0)
-    pdf1!.insert(page2!, at: 1)
-    return pdf1!
-}
-
-public func mergeTwoLongPDFs(file1: String, file2: String) -> PDFDocument {
-    let pdata1 = try! NSData(contentsOfFile: file1) as Data
-    let pdata2 = try! NSData(contentsOfFile: file2) as Data
-    let pdf1 = PDFDocument(data: pdata1)
-    let pdf2 = PDFDocument(data: pdata2)
-    let p2len = pdf2!.pageCount
-    var curpage = pdf1!.pageCount
-    var page: PDFPage
-    for i in 0..<p2len {
-        page = pdf2!.page(at: i)!
-        pdf1!.insert(page, at: curpage)
-        curpage+=1
-    }
-    return pdf1!
-}
-
 func openPDF(_ file: String) -> PDFDocument {
     let pdata = try! NSData(contentsOfFile: file) as Data
     let pdf = PDFDocument(data: pdata)
