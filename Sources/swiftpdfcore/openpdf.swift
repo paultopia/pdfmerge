89edeb8e37c3f6a6b5c8a63d7efb5d499ad62bb3
Paul Gowder <paul.gowder@gmail.com>
2019-01-13 18:14:14 -0600

    unwrap all the optionals to make it compile

--- Sources/swiftpdfcore/openpdf.swift
+++ Sources/swiftpdfcore/openpdf.swift
@@ -21,9 +21,9 @@ public func mergeSinglePagePDFs(file1: String, file2: String) -> PDFDocument {
     let pdata2 = try! NSData(contentsOfFile: file2) as Data
     let pdf1 = PDFDocument(data: pdata1)
     let pdf2 = PDFDocument(data: pdata2)
-    print(pdata1.pageCount)
-    let page2 = pdf2.page(at: 0)
-    pdf1.insert(page2!, at: 1)
-    return pdf1
+    print(pdf1!.pageCount)
+    let page2 = pdf2!.page(at: 0)
+    pdf1!.insert(page2!, at: 1)
+    return pdf1!
 
 }
