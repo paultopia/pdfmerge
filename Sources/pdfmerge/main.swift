66e8bcacd13a9322ac91f02b2c12598447b687ad
Paul Gowder <paultopia@users.noreply.github.com>
2019-01-16 22:51:04 -0600

    communicative errors

--- Sources/pdfmerge/main.swift
+++ Sources/pdfmerge/main.swift
@@ -1,15 +1,26 @@
 import pdfmergecore
 
-// "Cannot open \(file). It may not exist, or you may not have permissions for it."
+// "Cannot open \(filename). It may not exist, or you may not have permissions for it."
 // "Cannot rest list of files to merge. Is it a utf-8 encoded text file? It should be."
-// "Target file already exists. Aborting."
+// 
 
+
+case targetFileExists(filename: String)
+    case cannotOpenFile(filename: String)
+    case fileNotValidPDF(filename: String)
+    case cannotReadFileList(filename: String)
 
 let merger = PDFMerger()
 
 do {
     try merger.run()
+    } catch PDFMergeError.targetFileExists(let filename) {
+        print("Target file \(filename) already exists. Aborting.")
+    } catch PDFMergeError.cannotOpenFile(let filename) {
+        print("Cannot open file \(filename). It may not exist, or you may not have permissions for it. Aborting.")
+    } catch PDFMergeError.fileNotValidPDF(let filename) {
+        print("File \(filename) does not appear to be a valid PDF. Aborting.")
+    } catch PDFMergeError.cannotReadFileList(let filename) {
+        print("Cannot rest list of files to merge in \(filename). Is it a utf-8 encoded text file? It should be. Aborting")
     }
-catch is PDFMergeError {
-    print("errored, aborted")
-    }
+    
