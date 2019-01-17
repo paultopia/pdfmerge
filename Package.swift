8f3c32eeb00452355c7ecc37da3d5588b44f57fe
Paul Gowder <paultopia@users.noreply.github.com>
2019-01-16 21:04:17 -0600

    get rid of the require library from my code in prep to make errors sensible

--- Package.swift
+++ Package.swift
@@ -5,18 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "pdfmerge",
-    dependencies: [
-	.package(url: "git@github.com:johnsundell/require.git", from: "1.0.0")
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
       .target(
         	name: "pdfmerge",
         	dependencies: ["pdfmergecore"]),
 	.target(
 		name: "pdfmergecore",
-		dependencies: ["Require"]),
+		dependencies: []),
       	.testTarget(
         	name: "pdfmergetest",
         	dependencies: ["pdfmergecore"])
--- Sources/pdfmergecore/openpdf.swift
+++ Sources/pdfmergecore/openpdf.swift
@@ -1,6 +1,5 @@
 import Quartz
 import Foundation
-import Require
 
 func fileExists(_ filename: String) -> Bool {
     let fileManager = FileManager()
@@ -11,8 +10,10 @@ func openPDF(_ file: String) -> PDFDocument {
     guard let pdata = try? NSData(contentsOfFile: file) as Data else {
         preconditionFailure("Cannot open \(file). It may not exist, or you may not have permissions for it.")
     }
-    let pdf = PDFDocument(data: pdata)
-        return pdf.require(hint: "Cannot open PDF file \(file). It may not exist, or not be a well-formed PDF file. Aborting.")
+    guard let pdf = PDFDocument(data: pdata) else {
+        preconditionFailure("Cannot open PDF file \(file). It may not exist, or not be a well-formed PDF file. Aborting.")
+    }
+        return pdf
 }
 
 public func mergePDFs(files: [String]) -> PDFDocument {
@@ -37,15 +38,17 @@ public func mergePDFs(files: [String]) -> PDFDocument {
 
 func listPDFsInCurrentDirectory() -> [String]{
     let fileManager = FileManager()
-    let files = try? fileManager.contentsOfDirectory(atPath: ".")
+    guard let files = try? fileManager.contentsOfDirectory(atPath: ".") else {
+    preconditionFailure("Cannot open list of files to merge. Aborting.")
+    }
     return files
-      .require(hint: "Cannot open list of files to merge. Aborting.")
       .filter({ $0.hasSuffix(".pdf") }).sorted(by: <)
 }
 
 func getListFromFile(_ infile: String) -> [String]{
-    let text = try? String(contentsOfFile: infile, encoding: .utf8)
+    guard let text = try? String(contentsOfFile: infile, encoding: .utf8) else {
+    preconditionFailure("Cannot rest list of files to merge. Is it a utf-8 encoded text file? It should be.")
+    }
     return text
-      .require(hint: "Cannot rest list of files to merge. Is it a utf-8 encoded text file? It should be.")
       .split(separator: "\n").map(String.init) // because split returns an array of Substrings not of Strings annoyingly.
 }
